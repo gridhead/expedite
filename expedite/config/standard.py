5f6fde389ac8dd8c5af24480fdb53f7fc73a57d7
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2024-06-27 17:29:06 +0530

    Add configuration submodule
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

---
+++ expedite/config/standard.py
@@ -0,0 +1,69 @@
+"""
+expedite
+Copyright (C) 2024 Akashdeep Dhar
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Any Red Hat trademarks that are incorporated in the codebase or documentation
+are not subject to the GNU Affero General Public License and may only be used
+or replicated with the express permission of Red Hat, Inc.
+"""
+
+
+from logging import getLogger
+from logging.config import dictConfig
+from uuid import uuid4
+
+
+server_addr = "127.0.0.1"
+server_port = 8080
+
+client_addr = ""
+client_time = 15
+client_pswd = uuid4().hex[0:8].upper()
+client_plan = ""
+client_endo = ""
+client_file = ""
+client_iden = ""
+client_pair = False
+
+connection_dict = dict()
+connection_list = set()
+
+logrconf = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s %(message)s",
+            "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "formatter": "standard",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "root": {
+        "level": "INFO",
+        "handlers": ["console"],
+    },
+}
+
+dictConfig(logrconf)
+
+logger = getLogger(__name__)
